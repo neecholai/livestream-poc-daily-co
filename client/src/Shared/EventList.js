ea0c6846481e1516f6db30baab3c13a0f20d4e1e
Nicholai Hansen <nicholaihansen1993@gmail.com>
2020-03-25 15:58:29 -0700

    bug fix from EventDetailPage - no events fixed

--- client/src/Shared/EventList.js
+++ client/src/Shared/EventList.js
@@ -5,7 +5,7 @@ function EventList({ events, creatorName }) {
   return (
     <div className="my-2 col-lg-8">
       <h4>Other Upcoming Content from {creatorName}</h4>
-      {events ? (
+      {events.length > 0 ? (
         <ul className="list-unstyled ">
           {events.map(event => (
             <EventCard key={event.id} event={event} page="event" />
